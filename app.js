961e810cdc1bd4c7503ed2cee4cc86db83346fe8
Patrick Mooney <patrick@broadbean.com>
2016-11-03 09:41:04 +0000

    seperate util logic

--- app.js
+++ app.js
@@ -1,68 +1,40 @@
-var express = require('express')
-var fs = require('fs');
-var hbs = require('handlebars');
+var express = require('express');
+var utils = require('./lib/utils.js');
 
-fs.mkdirSync( './public' );
-fs.mkdirSync( './public/css' );
-fs.mkdirSync( './public/js' );
+[
+    './public',
+    './public/css',
+    './public/js'
+].forEach( function ( dir ) {
+    utils.makeDirIfNotExists( dir );
+});
 
-prepare_assets(
-    './public/js/vendor.js',
-    [
+utils.prepareAssets({
+    './public/js/vendor.js': [
         'node_modules/jquery/dist/jquery.min.js',
         'node_modules/jquery-ui-dist/jquery-ui.min.js'
-    ]
-);
-prepare_assets(
-    './public/css/vendor.css',
-    [
+    ],
+    './public/css/vendor.css': [
         'node_modules/jquery-ui-dist/jquery-ui.min.css'
-    ]
-);
-prepare_assets(
-    './public/css/app.css',
-    [
+    ],
+    './public/css/app.css': [
         'assets/css/app.css'
-    ]
-);
-prepare_assets(
-    './public/js/app.js',
-    [
+    ],
+    './public/js/app.js': [
         'assets/js/app.js'
     ]
-);
+});
 
-var templates = compile_templates();
+var templates = utils.compileTemplates( './templates' );
 
 var app = express();
+
 app.use('/static', express.static('public'))
+
 app.get('/', function (req, res) {
     res.send( templates['index']({ name: req.query.name }) );
 });
+
 app.listen(3000, function () {
     console.log( 'Server started.' );
 });
-
-function compile_templates() {
-    var t_path = './templates';
-    var templates = {};
-    fs.readdirSync( t_path ).forEach(
-        function( item ) {
-            var content = fs.readFileSync( `${t_path}/${item}`, 'utf8' );
-            var name = item.match(/(.+?)\.hbs/)[1];
-            templates[name] = hbs.compile( content );
-        }
-    );
-    return templates;
-}
-
-function prepare_assets( outFn, assets ) {
-    fs.writeFileSync(
-        outFn,
-        assets.map(
-            function( item ) {
-                return fs.readFileSync( item, 'utf8' );
-            }
-        ).join("\n\n")
-    );
-}
