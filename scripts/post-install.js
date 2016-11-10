11c6ba56c77c8040d84a1d5a74bb2f7b485e3732
Patrick Mooney <patrick@broadbean.com>
2016-11-10 19:32:52 +0000

    adding debug to build script

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -4,6 +4,8 @@ var fs = require('fs-extra');
 
 brunch.build({});
 
+console.log( 'BRUNCH BUILDING' );
+
 /* static dep images */
 var images = [
     './node_modules/jquery-ui/themes/base/images/ui-icons_444444_256x240.png',
@@ -16,5 +18,8 @@ if ( ! fs.existsSync(publicPath) ) { fs.mkdirSync(publicPath); }
 if ( ! fs.existsSync(imagePath) ) { fs.mkdirSync(imagePath); }
 images.forEach( function ( path ) {
     var matches = path.match(/\/([^\/]+)$/);
+    console.log( 'Copy image: ' + matches[1] );
     fs.copySync( path, imagePath+matches[1] );
 });
+
+console.log( 'finish' );
