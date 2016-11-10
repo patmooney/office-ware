0eb3040e10a164b2b6d3a9b6d76b301e0b0a98c7
Patrick Mooney <patrick@broadbean.com>
2016-11-10 19:27:26 +0000

    make sure public dir is created at the time of adding images

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -2,8 +2,6 @@
 var brunch = new require('brunch');
 var fs = require('fs-extra');
 
-console.log(process.cwd());
-
 brunch.build({});
 
 /* static dep images */
@@ -12,8 +10,10 @@ var images = [
     './node_modules/jquery-ui/themes/base/images/ui-icons_555555_256x240.png'
 ];
 
-var imagePath = 'public/images/';
-if ( ! fs.existsSync(imagePath) ) { fs.mkdirSync(imagePath) }
+var publicPath = './public/';
+var imagePath = publicPath + 'images/';
+if ( ! fs.existsSync(publicPath) ) { fs.mkdirSync(publicPath); }
+if ( ! fs.existsSync(imagePath) ) { fs.mkdirSync(imagePath); }
 images.forEach( function ( path ) {
     var matches = path.match(/\/([^\/]+)$/);
     fs.copySync( path, imagePath+matches[1] );
